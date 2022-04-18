453eb359a66f6546591989c272807e6201c3eaa2
gongyujin <96815817+gongyujin@users.noreply.github.com>
2022-04-18 18:15:43 +0900

    commit

--- ex0418/stumanage/studef.py
+++ ex0418/stumanage/studef.py
@@ -1,13 +1,11 @@
 from stumanage.student import Student
 import cx_Oracle
 
-
-
 stuSave=[]
 
 # 상단타이틀 출력
 def topTitle():
-    print('번호','이름','국어','영어','합계','평균','등수',sep='\t')  
+    print('번호','이름','국어','영어','수학','합계','평균','등수',sep='\t')  
     print('-'*60)
 
 # 화면출력
@@ -25,6 +23,7 @@ def screenPrint():
     choice=int(input('원하는 번호를 입력하세요.>> '))
 
     return choice
+
 
 # 학생성적입력
 def stuInput():
@@ -46,90 +45,128 @@ def stuInput():
             :1,:2,:3,:4,:5,:6,:7)"
         cs.execute(sql,(stuname,kor,eng,math,(kor+eng+math),(kor+eng+math)/3,1)) # sql구문 입력
         print("insert : ",cs.rowcount)
-        temp=Student(stuname, kor, eng)
-        stuSave.append(temp)
-        print('{}번. {} 학생이 저장되었습니다.'.format(temp.stuno,stuname))
-    
+        print('{} 학생이 저장되었습니다.'.format(stuname))
+        cs.close()
+        conn.commit()
+        conn.close()
+        print()
+
 
 # 학생전체출력
 def stuoutput():
     print()
     topTitle()
     
-    for stu in stuSave:
-        print(stu)
+    conn=cx_Oracle.connect("ora_user/1234@192.168.0.18:1521")
+    cs=conn.cursor()
+    sql="select * from studata"
+    rows=cs.execute(sql)
+    
+    for row in rows:
+        print(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],sep='\t')
+    sql="select count(*) from studata"
+    cs.execute(sql)
+    count=cs.fetchone() # 메모리에 담긴 데이터를 한행씩 fetch한다
+    print("{}명의 데이터가 검색되었습니다.".format(count[0]))
     print()
+    cs.close()
+    conn.close()
+    
     
 # 학생검색출력
 def stuSearch():
     print()
     print('[ 학생검색출력 ]')
-    searchName=input('검색할 학생이름을 입력하세요.>> ')
-
+    searchName=input('검색할 학생이름을 입력하세요.>> ').lower()
+    # db연결
+    conn = cx_Oracle.connect("ora_user/1234@localhost:1521/xe")
+    cs=conn.cursor()
+    sql="select * from studata"
+    rows=cs.execute(sql)
+    
     count=0
-    for stu in stuSave:
-        if stu==searchName:
+    for row in rows:
+        if row[1].lower()==searchName:
             topTitle()
-            print(stu)
+            print(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],sep='\t')
             count=1
             break
     if count==0:
         print('검색된 이름이 없습니다. 다시 한번 입력하세요.!!')
 
 
+
 # 학생성적수정
 def stuModify():
     print()
     print('[ 학생성적수정 ]')
-    searchName=input('수정할 학생이름을 입력하세요.>> ')
-
     count=0
-    for stu in stuSave:
-        if stu == searchName:
+    searchName=input('수정할 학생이름을 입력하세요.>> ')
+    conn = cx_Oracle.connect("ora_user/1234@localhost:1521/xe")
+    cs=conn.cursor()
+    sql="select * from studata"
+    rows=cs.execute(sql)
+    
+    for row in rows:
+        if row[1] == searchName:
             print('{}의 학생이 검색되었습니다.'.format(searchName))
  
             print('[ 성적수정 ]')
             print('1. 국어점수 수정')
             print('2. 영어점수 수정')
+            print('3. 수학점수 수정')
             tempNum=int(input('수정과목 번호를 입력하세요.>> '))
             if tempNum==1:
-                print('현재 국어점수 : {}'.format(stu.kor))
+                print('현재 국어점수 : {}'.format(row[2]))
                 score=int(input('수정할 국어점수를 입력하세요.>> '))
-                stu.setKor(score)
+                sql="update studata set kor=:1,total=:2,avg=:3 where stuno=:4"
+                cs.execute(sql,(score,(score+row[3]+row[4]),(score+row[3]+row[4])/3,row[0]))
+                cs.close()
+                conn.commit()
+                conn.close()
                 print('국어점수가 {}으로 변경되었습니다.'.format(score))
-
-            elif tempNum==2:
-                print('현재 영어점수 : {}'.format(stu.eng))
-                score=int(input('수정할 영어점수를 입력하세요.>> '))
-                stu.setEng(score)
-                print('영어점수가 {}으로 변경되었습니다.'.format(score))
-                    
+   
             count=1
             break
             
     if count==0:
         print('검색된 이름이 없습니다. 다시 한번 입력하세요.!!')    
                 
+                
+                
 # 학생성적 삭제
 def stuDelete():
     print()
     print('[ 학생성적삭제 ]')
-    searchName=input('삭제할 학생이름을 입력하세요.>> ')
-    for i, stu in enumerate(stuSave):
-        if stu == searchName:
-            print('{}의 학생이 검색되었습니다.'.format(searchName))
-            flag=input('정말 삭제하시겠습니까? ')
-            if flag == 'y' or flag == 'Y':
-                del(stuSave[i])
-                print('{} 학생이 삭제되었습니다.'.format(searchName))
-                
-            else:
-                print('삭제가 취소되었습니다.')
+    searchName=input('삭제할 학생이름을 입력하세요.>> ').lower()
+    conn = cx_Oracle.connect("ora_user/1234@localhost:1521/xe")
+    cs=conn.cursor()
+    sql="select * from studata"
+    rows=cs.execute(sql)
+    # 찾은 데이터를 출력
+    count=0
+    for row in rows:
+        if searchName in row[1].lower():
+            print(row[0],row[1],sep='\t')
             count=1
-            break
-        
-    if count==0:
+    
+    if count!=0:    
+        print('{}의 학생이 검색되었습니다.'.format(searchName))
+        flag=input('삭제할 번호를 입력하세요. (0.삭제취소)>> ')
+        if flag != '0':
+            sql="delete studata where stuno="+flag
+            cs.execute(sql)
+            print('학생이 삭제되었습니다.')
+        else:
+            print('삭제가 취소되었습니다.')
+  
+    else:
         print('검색된 이름이 없습니다. 다시 한번 입력하세요.!!')               
+    cs.close()
+    conn.commit()
+    conn.close()
+        
+        
         
 # 학생듣수 처리
 def stuRank():
