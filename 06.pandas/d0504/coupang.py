1f09c738d067f8e726ca3090d80bf3d0a4946bd9
gongyujin <96815817+gongyujin@users.noreply.github.com>
2022-05-04 18:20:43 +0900

    commit

--- 06.pandas/d0504/coupang.py
+++ 06.pandas/d0504/coupang.py
@@ -21,15 +21,38 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
 options=webdriver.ChromeOptions()
-options.headless=True
-options.add_argument('window-size=1920x1080')
-options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36')
 browser=webdriver.Chrome(options=options)
-
-
 options.add_experimental_option('detach',True)
+browser.maximize_window()
+browser.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", { "source": """ Object.defineProperty(navigator, 'webdriver', { get: () => undefined }) """ })
 
-url='https://www.coupang.com/'
+
+
+# url='https://www.coupang.com/'
+# browser.get(url)
+# time.sleep(2)
+
+
+# prev_height=browser.execute_script("return document.body.scrollHeight")
+# while True:
+#     browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+#     time.sleep(5)
+#     curr_height = browser.execute_script("return document.body.scrollHeight")
+#     if prev_height == curr_height:
+#         break # 스크롤 크기가 더 이상 변경이 없을시 종료
+#     prev_height = curr_height
+
+   
+# soup=BeautifulSoup(browser.page_source,'lxml')
+# articles=soup.find('div',{'id':'categoryBestUnit'}).find_all('div',{'class':'category-best-unit'})
+
+# for i,article in enumerate(articles):
+
+#     print(article)
+#     break
+
+
+url='https://www.coupang.com/np/categories/178255?traid=home_CategoryBest_quick_link'
 browser.get(url)
 time.sleep(2)
 
@@ -37,13 +60,17 @@ time.sleep(2)
 prev_height=browser.execute_script("return document.body.scrollHeight")
 while True:
     browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-    time.sleep(2)
+    time.sleep(5)
     curr_height = browser.execute_script("return document.body.scrollHeight")
     if prev_height == curr_height:
         break # 스크롤 크기가 더 이상 변경이 없을시 종료
     prev_height = curr_height
 
-time.sleep(10)    
+   
 soup=BeautifulSoup(browser.page_source,'lxml')
-articles=soup.find('div',{'id':'container'}).find('section',{'id':'contents'})
-print(articles)
+articles=soup.find('ul',{'id':'productList'}).find_all('li')
+for i,article in enumerate(articles):
+
+    print(article)
+    if i>=5:
+        break
--- 07.plt/p0504/p0504_07.py
+++ 07.plt/p0504/p0504_07.py
@@ -10,9 +10,10 @@ df=pd.read_excel('movie.xlsx')
 
 x=df['영화']
 y=df['평점']
-plt.plot(x,y,'ro:',label='평점')
-plt.legend(loc=('upper right'))
+# 선-두께,색상,모양,marker ==> 크기, 테두리색,바탕색,모양
+plt.plot(x,y,'ro',ms=10,label='평점',linestyle='none')
+plt.legend(loc=('lower left'))
 plt.title('흥행영화 관객그래프')
-plt.xlabel('영화제목')
-plt.ylabel('평점')
+plt.xlabel('영화제목',color='r',loc='center')
+plt.ylabel('평점',color='b',loc='center')
 plt.show()
